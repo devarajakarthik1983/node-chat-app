10f0bff6b720094b3a02562adc4e530fe58e90b0
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-01-15 12:19:41 -0500

    Added two way connection from server to client

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO =require('socket.io')
 
 const publicPath = path.join(__dirname , '../public');
-
+const port = process.env.PORT || 3000;
 var app = express();
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-port = process.env.PORT || 3000;
+io.on('connection' ,(socket)=>{
+  console.log('New user is connected');
 
-app.listen(3000,()=>{
+  socket.on('disconnect' ,()=>{
+    console.log('User was disconnected');
+  });
+});
+
+
+
+
+server.listen(3000,()=>{
   console.log(`Server started at ${port}`);
 });
