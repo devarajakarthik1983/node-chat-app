75265425b22979380c7f8b54dacc7ee9befbbb17
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-01-17 11:11:47 -0500

    Added geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,19 @@ socket.on('newMessage' , function (newMessage){
   var li =jQuery('<li></li>');
   li.text(`${newMessage.from}: ${newMessage.text}`);
   jQuery('#messages').append(li);
-})
+});
+
+
+socket.on('newLocationMessage' , function (newMessage){
+  // console.log('New Message' , newMessage);
+  var li =jQuery('<li></li>');
+  var a =jQuery('<a target="_blank">My Location</a>');
+  li.text(`${newMessage.from}: `);
+  a.attr('href',newMessage.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 
 // socket.emit('createMessage',{
 //   from:'test@test.com',
@@ -35,4 +47,23 @@ jQuery('#message-form').on('submit' , function(e) {
   } , function (){
 
   });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click' , function() {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by the browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // console.log(position);
+    socket.emit('createLocationMessage' ,{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude,
+    })
+   } , function() {
+    alert('Unable to fetch the location');
+  })
+
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,22 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message')
+var {generateMessage,generateLocationMessage} = require('./message')
 
 describe('Generate message' , ()=>{
   it('it should generate correct message object' ,(done)=>{
     var text =  generateMessage('kar@qw.com','test');
     expect(text.from).toBe('kar@qw.com')
     expect(text.text).toBe('test')
+    expect(typeof text.createdAt).toBe('number')
+    done();
+  });
+});
+
+describe('Generate location message' , ()=>{
+  it('it should generate correct location message' ,(done)=>{
+    var text =  generateLocationMessage('Admin',20,30);
+    expect(text.from).toBe('Admin')
+    expect(text.url).toBe('https://www.google.com/maps?q=20,30')
     expect(typeof text.createdAt).toBe('number')
     done();
   });
